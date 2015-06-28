8b59ae352b68f0621c32eeb43788411f41170f45
Kim Landeweer <krelandeweer@gmail.com>
2015-06-28 11:24:55 +0200

    Initial Commit

---
+++ Smoke Savings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Smoke Savings
+//
+//  Created by Kim on 28/06/15.
+//  Copyright (c) 2015 Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
